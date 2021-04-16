00d804f2b788106c0466a553813c900fd1684b78
andreasgrum00 <55033182+andreasgrum00@users.noreply.github.com>
2021-04-15 22:54:51 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,8 +1,6 @@
 const Discord = require('discord.js');
 const { readdirSync } = require('fs');
 const { join } = require('path');
-const avatares = ['https://i.imgur.com/b4BosST.png', 'https://i.imgur.com/f5jR6kg.png', 'https://pbs.twimg.com/profile_images/709356708/barney_icon_400x400.jpg'];
-const avatar = avatares[Math.floor(Math.random() * avatares.length)];
 
 const config = require('./config.json');
 
@@ -22,24 +20,12 @@ bot.on('ready', () => {
     bot.user.setActivity('!!help');
 
     console.log(`O Bot foi logado como ${bot.user.tag}`);
-    
-    var interval = setInterval (function () {
-        bot.user.setAvatar(avatar)
-        .catch(console.error);
-    }, 300000);
-});
-
-bot.on('guildMemberAdd', member => {
-    const embed = new Discord.MessageEmbed()
-    .setTitle(`Bem-vindo(a), ${member.user.tag}`)
-    .setAuthor(bot.user.tag)
 });
 
 bot.on('message', async message => {
-    const grumm = bot.users.cache.get('417829177757007872');
 
     if(message.author.bot) return;
-    if(message.channel.type === 'dm') return grumm.send(`${message.author.tag} (id: ${message.author.id}) disse: "${message.content}"`);
+    if(message.channel.type === 'dm') return;
     if(!message.content.startsWith(prefix)) return;
 
     if(message.content.startsWith(prefix)) {
